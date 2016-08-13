6c5f355dc03b1c994032a1151e3504acf7c7d7a3
AlexGerman1 <alexgerman11233@gmail.com>
2016-08-12 20:11:40 -0700

    Finished functionality and added docstrings. can now be called from terminal by typing 'mailroom'

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ from setuptools import setup
 
 setup(
     name="mailroom",
-    description="This package allows the user to update an existing list of donors," \
-                        "to generate a Thank-You Letter,  and to create a report",
+    description="This package allows the user to update an existing list of"
+                "donors, to generate a Thank-You Letter,"
+                "and to create a report",
     version=0.1,
     license='MIT',
     author="Alex German, Tatiana Weaver",
     author_email="Alexgerman11233@gmail.com, tanya83@uw.edu",
     py_modules=['mailroom'],
-    package_dir={' ' : 'src'},
+    package_dir={' ': 'src'},
     install_requires=[],
-    extras_require={'test':['pytest', 'pytest-watch', 'tox']},
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
     entry_points={
         'console_scripts': [
-            'mailroom=mailroom:main'
+            'mailroom=src.mailroom:main_function'
         ]
     }
 )
